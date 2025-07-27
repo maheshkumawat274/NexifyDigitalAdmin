437c5a44360e51015d5c6e2ce4c975174a09de08
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-07-28 03:45:14 +0530

    finally done

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: "/admin/", // 👈 very important for correct asset paths
   plugins: [
     react(),
     tailwindcss()
-
   ],
 })
+
